fd246a09d5c214a5b4ab4da538954db945bd9754
Bruno Macedo <devbrunomacedo@gmail.com>
2024-05-09 22:59:03 -0300

    feat: Exemplos referentes as aulas Tipos e Variaveis

---
+++ Java-Sintaxe-Tipos/src/edu/bruno/tipos/TiposVariaveis.java
@@ -0,0 +1,13 @@
+package edu.bruno.tipos;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 1407.50;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        final int ESTADOS_BRASIL = 27; // tornar uma constante  
+        System.out.println(ESTADOS_BRASIL); 
+    }
+}
